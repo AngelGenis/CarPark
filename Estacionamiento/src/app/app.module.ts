02e499ec6cefc20ee0677e95f9ab1b8b0abc32f7
camvaz <camposvazquezv@gmail.com>
2019-11-12 17:05:39 -0600

    Separado en componentes, login agregado, botones circulares, botones de redes sociales en el footer

--- Estacionamiento/src/app/app.module.ts
+++ Estacionamiento/src/app/app.module.ts
@@ -6,11 +6,23 @@ import { AppComponent } from './app.component';
 
 // MDBootstrap
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    IntroComponent,
+    LoginComponent,
+    NavbarComponent,
+    AboutComponent,
+    InstalacionesComponent
   ],
   imports: [
     BrowserModule,
