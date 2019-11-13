fe40257541583ca79dd2ee37e07c417f095df3cb
camvaz <camposvazquezv@gmail.com>
2019-11-13 11:32:05 -0600

    Commit de revision

--- Estacionamiento/src/app/about/about.component.ts
+++ Estacionamiento/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-about',
@@ -11,5 +12,4 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
