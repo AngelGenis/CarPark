ef8d1bfb362edfbabf1274db618d91d59e66a8fb
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-11-28 00:09:44 -0600

    login

--- Estacionamiento/src/app/app-routing.module.ts
+++ Estacionamiento/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PresentacionComponent } from './presentacion/presentacion.component';
 import { ReservarComponent } from './reservar/reservar.component';
-
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: PresentacionComponent},
   { path: 'reservar', component: ReservarComponent},
-  { path: 'inicio', component: PresentacionComponent}
+  { path: 'inicio', component: PresentacionComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
