51776eada5036046ad9528e101914d409fe27f62
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-11-28 15:12:53 -0600

    Interfaz Reservar

--- Estacionamiento/src/app/reservar/reservar.component.ts
+++ Estacionamiento/src/app/reservar/reservar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-reservar',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservar.component.css']
 })
 export class ReservarComponent implements OnInit {
+  banderaAutos: boolean = true;
+  banderaTarjetas: boolean = true;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickAutos(){
+    if(this.banderaAutos == true){
+      $("#AutosRegistrados").show();
+      document.getElementById('ArrowVehiculos').classList.add('up');
+      document.getElementById('ArrowVehiculos').classList.remove('down');
+
+
+      this.banderaAutos = false;
+    }else{
+      $("#AutosRegistrados").hide();
+      this.banderaAutos = true;
+      document.getElementById('ArrowVehiculos').classList.add('down');
+      document.getElementById('ArrowVehiculos').classList.remove('up');
+      
+    }
+    
+  }
+  onClickTarjetas(){
+    if(this.banderaTarjetas == true){
+      $("#TarjetasRegistradas").show();
+      this.banderaTarjetas = false;
+    }else{
+      $("#TarjetasRegistradas").hide();
+      this.banderaTarjetas = true;
+    }
   }
 
 }
