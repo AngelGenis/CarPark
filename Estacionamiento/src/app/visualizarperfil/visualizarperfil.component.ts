c0644964d914c749a8acd4c7ce904db5539ad298
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-11-28 23:59:30 -0600

    cambios en perfil

--- Estacionamiento/src/app/visualizarperfil/visualizarperfil.component.ts
+++ Estacionamiento/src/app/visualizarperfil/visualizarperfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-visualizarperfil',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizarperfil.component.css']
 })
 export class VisualizarperfilComponent implements OnInit {
+  public bandcontenedor: number = 1;
+
 
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  onClickDatosPersonales(){
+    this.bandcontenedor = 1;
+  }
+  onClickDatosAutos(){
+    this.bandcontenedor = 2;
+  }
+  onClickDatosTarjetas(){
+    this.bandcontenedor = 3;
+  }
+
 
 }
