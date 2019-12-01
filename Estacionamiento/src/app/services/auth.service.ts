dabe8553b5d672280f6807abfee5b6c8e8fed3f2
camvaz <camposvazquezv@gmail.com>
2019-12-01 10:10:27 -0600

    Animacion hamburger menu

--- Estacionamiento/src/app/services/auth.service.ts
+++ Estacionamiento/src/app/services/auth.service.ts
@@ -42,11 +42,14 @@ export class AuthService {
 
   public logearUsuario(email, pass){
     return this.afAuth.auth.signInWithEmailAndPassword(email,pass).then(res=>{
-
                 $("#Menu1").hide();
                 $(".menu-btn").show();
-      this.toastr.success('Sesion Iniciada',`Bienvenido: ${res.user.displayName}`)
-      this.router.navigate(['/transicionlog','in']);
+                setTimeout(()=>{
+                  $(".menu-btn").addClass('animated');
+                  $(".menu-btn").addClass('heartBeat');
+              },3000)
+                this.toastr.success('Sesion Iniciada',`Bienvenido: ${res.user.displayName}`)
+                this.router.navigate(['/transicionlog','in']);
     }).catch(e=>{
       this.toastr.error('Error','No se encuentra registrado')
     })
