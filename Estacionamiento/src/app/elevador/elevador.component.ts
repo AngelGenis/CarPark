05df022817a796f35789132fa8c92bb4fa15caf9
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-11-30 23:37:16 -0600

    animacion elevador y croquis

--- Estacionamiento/src/app/elevador/elevador.component.ts
+++ Estacionamiento/src/app/elevador/elevador.component.ts
@@ -8,33 +8,64 @@ import * as $ from 'jquery';
 })
 export class ElevadorComponent implements OnInit {
 
-  public piso: number = 3;
+  public piso: number = 1;
   constructor() { }
 
   ngOnInit() {
   }
 
-  onClickSubir(){
-    if(this.piso==1){
+  onClickSubir() {
+    if (this.piso == 1) {
       $("#RectElevador").addClass("subirprimero");
+      this.carro();
+      this.escenario();
     }
-    if(this.piso==2){
+    if (this.piso == 2) {
       $("#RectElevador").addClass("subirsegundo");
+      this.carro();
+      this.escenario();
     }
-    if(this.piso==3){
+    if (this.piso == 3) {
       $("#RectElevador").addClass("subirtercero");
+      this.carro();
+      this.escenario();
     }
   }
-  onClickBajar(){
-    if(this.piso==1){
+  
+  /*onClickBajar() {
+    if (this.piso == 1) {
       $("#RectElevador").addClass("bajar1");
     }
-    if(this.piso==2){
+    if (this.piso == 2) {
       $("#RectElevador").addClass("bajar2");
     }
-    if(this.piso==3){
+    if (this.piso == 3) {
       $("#RectElevador").addClass("bajar3");
     }
+  }*/
 
+  carro() {
+    setTimeout(function () {
+      $(".img").addClass("imgefecto");
+      $(".img").addClass("moverautoinicio");
+      $(".piso").addClass("esc");
+      $(".piso").addClass("escdes");
+    }, 2000);
   }
+
+  escenario() {
+    setTimeout(function () {
+      $("#Con").addClass("desaparecer");
+      setTimeout(function () {
+        $("#Con2").css("opacity", "1");
+        $("#Con").css("opacity", "0");
+        setTimeout(function () {
+          $(".img2").addClass("carroap");
+          $(".img2").css("left", "-25px");
+        }, 100);
+      }, 200);
+    }, 2000);
+  }
+
+
 }
