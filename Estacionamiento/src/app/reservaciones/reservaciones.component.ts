f9fa423571aeb41b1f93bb49e19a29541e5687ef
AngelGenis <38354735+AngelGenis@users.noreply.github.com>
2019-12-02 02:44:04 -0600

    Temporizador hecho

--- Estacionamiento/src/app/reservaciones/reservaciones.component.ts
+++ Estacionamiento/src/app/reservaciones/reservaciones.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservaciones.component.css']
 })
 export class ReservacionesComponent implements OnInit {
+  tiempo: number = 3;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickTimer(){
+    var horas = this.tiempo-1;
+    var minutos = 59;
+    var segundos = 59;
+
+    var x = setInterval(function() {
+        if(horas != 0 && minutos != 0 && segundos != 0){
+
+          if(segundos > 1){
+            segundos --;
+          }else if(minutos > 1){
+            minutos --;
+            segundos = 59;
+          }else if(horas > 1){
+            horas--;
+            minutos=59;
+          }  
+          document.getElementById("CountDown").innerHTML = horas + " : " + minutos + " : "+ segundos;
+
+        }
+        if(horas == 1 && minutos == 1 && segundos == 1){
+          clearInterval(x);
+           document.getElementById("CountDown").innerHTML = "EXPIRED";
+        }
+   }, 1000);
+    
   }
 
 }
